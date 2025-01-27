4dadd5b9f6792847d617f97b7c8428724a7bf859
monster-1951 <shivashankarsince19@gmail.com>
2025-01-27 20:22:33 +0530

    added position im employee model

--- Database/models/Employee.model.js
+++ Database/models/Employee.model.js
@@ -2,50 +2,54 @@ import mongoose, { Mongoose, Schema } from "mongoose";
 
 const EmployeeSchema = new Schema(
   {
-    id:{
-      type:Number,
-      unique:true,
-      required:[true,"Employee ID is required"]
+    id: {
+      type: Number,
+      unique: true,
+      required: [true, "Employee ID is required"],
+    },
+    position:{
+      type:String,
+      required:[true, "Employee position is required"],
     },
     name: {
-        type: String,
-        required: [true, "Please provide your name"],
+      type: String,
+      required: [true, "Please provide your name"],
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Please provide a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: {
+        values: ["Employee", "Admin"],
+        message: "Please select your role",
       },
-      email: {
-        type: String,
-        unique:true,
-        required: [true, "Please provide a valid email"],
-      },
-      password: {
-        type: String,
-        required: [true, "Please provide a password"],
-        select: false,
-      },
-      role: {
-        type: String,
-        enum: {
-          values: ["Employee", "Admin"],
-          message: "Please select your role",
+      default: "Employee",
+    },
+    attendance: [
+      {
+        date: {
+          type: String,
+          required: true,
         },
-        default: "Employee",
-      },
-      attendance: [
-        {
-          date: {
-            type: String,
-            required: true,
-          },
-          status: {
-            type: String,
-            required: true,
-            enum: ['Present', 'Absent', 'Leave'], // Ensure the status is one of these values
-          },
+        status: {
+          type: String,
+          required: true,
+          enum: ["Present", "Absent", "Leave"], // Ensure the status is one of these values
         },
-      ],
-      salary: {
-        type: Number,
-        required: true,
       },
+    ],
+    salary: {
+      type: Number,
+      required: true,
+    },
   },
   { timestamps: true }
 );
